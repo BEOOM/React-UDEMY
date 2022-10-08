d254ebc6597dbbee3c050323906151bb44e1596c
BEOOM <wnsqja0512@naver.com>
2022-10-08 12:46:06 +0900

    폼 양식 추가하기

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 【한글자막】 React 완벽 가이드 with Redux, Next.js, TypeScript
-ㅎ
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -26,7 +27,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       {/* <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
