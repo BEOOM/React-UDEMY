af610c881eb04138f67451ba4ca05bff5ae702b2
BEOOM <wnsqja0512@naver.com>
2022-10-21 12:38:28 +0900

    #101 입력 state 관리하기

--- src/components/UI/P1Button.js
+++ src/components/UI/P1Button.js
@@ -3,13 +3,13 @@ import classes from "./P1Button.module.css";
 
 const P1Button = (props) => {
   return (
-    <div
+    <button
       className={classes.button}
       type={props.type || "button"}
       onClick={props.onClick}
     >
       {props.children}
-    </div>
+    </button>
   );
 };
 export default P1Button;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import P1Card from "../UI/P1Card";
 import classes from "./AddUser.module.css";
 import P1Button from "../UI/P1Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (e) => {
     e.preventDefault();
+    console.log(enteredUsername, enteredAge);
   };
+
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+
+  const ageChangeHandler = (e) => {
+    setEnteredAge(e.target.value);
+  };
+
   return (
     <P1Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
+        <input type="text" id="username" onChange={usernameChangeHandler} />
         <label htmlFor="age">Age</label>
-        <input type="number" id="age" />
+        <input type="number" id="age" onChange={ageChangeHandler} />
         <P1Button type="submit">Add User</P1Button>
       </form>
     </P1Card>
