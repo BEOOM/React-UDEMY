fc6b541e8164a3b27d3386795f1bf52f0a6f947c
BEOOM <wnsqja0512@naver.com>
2022-10-21 11:56:25 +0900

    #98 사용자 컴포넌트 추가하기

---
+++ src/Practice1.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Practice1.css";
+import AddUser from "./components/Users/AddUser";
+
+const Practice1 = () => {
+  return (
+    <div>
+      <AddUser />
+    </div>
+  );
+};
+
+export default Practice1;
