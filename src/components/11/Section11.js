fbb448635c0b6f65ba211802d17ebd42b7fe331a
BEOOM <wnsqja0512@naver.com>
2022-11-02 10:29:34 +0900

    #141 Header

---
+++ src/components/11/Section11.js
@@ -0,0 +1,12 @@
+import Header from "./Layout/Header";
+import { Fragment } from "react";
+
+function Section11() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default Section11;
