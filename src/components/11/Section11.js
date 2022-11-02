387357d93b3b0002507209a42cf130e8cbb4db3f
BEOOM <wnsqja0512@naver.com>
2022-11-02 10:46:54 +0900

    #144 Meals Component

--- src/components/11/Section11.js
+++ src/components/11/Section11.js
@@ -1,10 +1,14 @@
 import Header from "./Layout/Header";
 import { Fragment } from "react";
+import Meals from "./Meals/Meals";
 
 function Section11() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
