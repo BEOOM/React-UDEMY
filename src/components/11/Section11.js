7b4d506134fa624ea20fa2722c1b61e1aa5d1d4a
BEOOM <wnsqja0512@naver.com>
2022-11-05 10:59:31 +0900

    #148 Portal Modal

--- src/components/11/Section11.js
+++ src/components/11/Section11.js
@@ -1,10 +1,12 @@
 import Header from "./Layout/Header";
 import { Fragment } from "react";
 import Meals from "./Meals/Meals";
+import Cart from "./Cart/Cart";
 
 function Section11() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
