486bc055913ebbef6fb59fbfc5a794a1132035c9
BEOOM <wnsqja0512@naver.com>
2022-11-05 12:24:29 +0900

    #152 Ref / ForwardRef

--- src/components/11/Meals/MealItem/MealItemForm.js
+++ src/components/11/Meals/MealItem/MealItemForm.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setIsAmountValid] = useState(true);
+  const amountInputRef = useRef();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setIsAmountValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount",
           type: "number",
           min: "1",
           max: "5",
-          step:'1',
-          defaultValue:'1'
+          step: "1",
+          defaultValue: "1",
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>1-5 사이의 숫자를 입력하세요</p>}
     </form>
   );
 };
