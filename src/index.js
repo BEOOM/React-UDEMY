6b09fbe55ce27a78701b9ca429d9d85f990be828
BEOOM <wnsqja0512@naver.com>
2022-11-16 16:41:13 +0900

    #185 Get 요청 보내기

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index13.css";
+import "./index14.css";
 import App from "./App";
 import App2 from "./App2";
 import Practice1 from "./Practice1";
@@ -8,6 +8,7 @@ import Section10 from "./Section10";
 import { AuthContextProvider } from "./components/10/store/auth-context";
 import Section11 from "./components/11/Section11";
 import Section13 from "./components/13/Section13";
+import Section14 from "./components/14/Section14";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Section13 />);
+root.render(<Section14 />);
