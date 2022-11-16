8c7ee9c9d3688ad899f6f3e2ce95f1d6682e21a0
BEOOM <wnsqja0512@naver.com>
2022-11-16 14:07:18 +0900

    #173 클라스 기반 컴포넌트 만들기

---
+++ src/components/13/Section13.js
@@ -0,0 +1,11 @@
+import Users from './components/Users';
+
+function Section13() {
+  return (
+    <div>
+      <Users />
+    </div>
+  );
+}
+
+export default Section13;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./index13.css";
 import App from "./App";
 import App2 from "./App2";
 import Practice1 from "./Practice1";
 import Section10 from "./Section10";
 import { AuthContextProvider } from "./components/10/store/auth-context";
 import Section11 from "./components/11/Section11";
+import Section13 from "./components/13/Section13";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Section11 />);
+root.render(<Section13 />);
