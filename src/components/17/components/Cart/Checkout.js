5eb3bd0e65f6e331213f408346e4f5a548544902
BEOOM <wnsqja0512@naver.com>
2022-11-22 12:27:43 +0900

    #230 폼 값 가져오기/읽기

--- src/components/17/components/Cart/Checkout.js
+++ src/components/17/components/Cart/Checkout.js
@@ -1,30 +1,50 @@
+import { useRef } from "react";
 import classes from "./Checkout.module.css";
+
+// 1. 키를 누를때마다 스테이트 없데이트 2.폼 전체가 제출될 때 한번에 얻기
+//useRef를 통해 폼이 제출되었을 때만 정보 가져오기
+
+
 const Checkout = (props) => {
-  const confirmHandler = (e) => {
-    e.preventDefault();
+  const nameInputRef = useRef(); 
+  const streetInputRef = useRef();
+  const postalInputRef = useRef();
+  const cityInputRef = useRef();
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostalCode = postalInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+    //.current -> 입력된 실제 값에 접근할 수 있게 해준다
+    //.value -> 자바스크립트는 value property가 있어 접근할 수 있도록 해줌
   };
+
   return (
-    <form onSubmit={confirmHandler}>
+    <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
-        <label htmlFor="name">Your name</label>
-        <input type="text" id="name" />
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor="name">Your Address</label>
-        <input type="text" id="street" />
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" ref={streetInputRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor="name">Postal Code</label>
-        <input type="text" id="postal" />
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" ref={postalInputRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor="name">City</label>
-        <input type="text" id="city" />
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityInputRef} />
       </div>
-      <button type="button" onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
