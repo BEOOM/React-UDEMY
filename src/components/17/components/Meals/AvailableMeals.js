c2df65e115a746d2f52c5839d83ecdac44e8b908
BEOOM <wnsqja0512@naver.com>
2022-11-22 11:34:36 +0900

    #227 로딩화면 보여주기

--- src/components/17/components/Meals/AvailableMeals.js
+++ src/components/17/components/Meals/AvailableMeals.js
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
@@ -22,9 +24,18 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
   const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
