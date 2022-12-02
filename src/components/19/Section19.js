9b4c2d8f0f80abf268e5881161cac1770bca851c
BEOOM <wnsqja0512@naver.com>
2022-12-02 19:20:38 +0900

    #264 redux 복습2

--- src/components/19/Section19.js
+++ src/components/19/Section19.js
@@ -13,6 +13,8 @@ function Section19() {
     </Layout>
   );
 }
+//useSelector <- 상태를 받는 함수를 전닳해야함 , 현재 데이터를 받고 컴포넌트에서 사용하고 싶은 데이터를 반환해야 한다
+//현재 슬라이드에서는 cartIsVisible 값이다 -> store/index.js -> ui key에 접근해서 사용해야 함
 //uiSlice value에 따른 cart 보여주기
 
 export default Section19;
--- src/components/19/components/Shop/Products.js
+++ src/components/19/components/Shop/Products.js
@@ -1,16 +1,43 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "b1",
+    price: 6,
+    title: "Book",
+    description: "First",
+  },
+  {
+    id: "b2",
+    price: 7,
+    title: "Ace",
+    description: "Second",
+  },
+  {
+    id: "b3",
+    price: 10,
+    title: "Plage",
+    description: "Pig",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => {
+          return (
+            <ProductItem
+              key={product.id}
+              title={product.title}
+              price={product.price}
+              description={product.description}
+              id={product.id}
+            />
+          );
+        })}
       </ul>
     </section>
   );
