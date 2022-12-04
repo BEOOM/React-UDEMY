b6ecfba7e22cd8825451cf9629cccde2e7d249ed
BEOOM <wnsqja0512@naver.com>
2022-12-04 12:34:52 +0900

    #270 HTTP Request + Redux

--- src/components/19/Section19.js
+++ src/components/19/Section19.js
@@ -1,27 +1,81 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, Fragment } from "react";
+import Notification from "./components/UI/Notification";
+import { uiActions } from "./store/ui-slice";
+import { useSelector, useDispatch } from "react-redux";
 // -> 데이터 추출하기
 
+let isInitial = true;
+
 function Section19() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const notification = useSelector((state) => state.ui.notification);
   const cart = useSelector((state) => state.cart);
   //카트의 변화를 감지
 
   useEffect(() => {
-    fetch("https://react-http-92230-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "sending",
+          message: "sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://react-http-92230-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("sending cart data failed");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "success",
+          message: "sent cart data successfully!",
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "error",
+          message: "sending cart data failed",
+        })
+      );
     });
-  }, [cart]);
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 //useSelector <- 상태를 받는 함수를 전닳해야함 , 현재 데이터를 받고 컴포넌트에서 사용하고 싶은 데이터를 반환해야 한다
--- src/components/19/store/ui-slice.js
+++ src/components/19/store/ui-slice.js
@@ -3,16 +3,24 @@ import { createSlice } from "@reduxjs/toolkit";
 const uiSlice = createSlice({
   name: "ui",
   initialState: {
-    cartIsVisible: false},
-    reducers: {
-      toggle(state) {
-        state.cartIsVisible = !state.cartIsVisible;
-        //기존 상태를 조작하는 것이 아닌 새 상태를 생성하여 변경
-      },
+    cartIsVisible: false,
+    notification: null,
+  },
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
+      //기존 상태를 조작하는 것이 아닌 새 상태를 생성하여 변경
     },
-  
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
 });
 
-export const uiActions = uiSlice.actions
+export const uiActions = uiSlice.actions;
 
 export default uiSlice;
