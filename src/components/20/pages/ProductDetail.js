08357f2bec62a69a602ea4f0b6e74245164e8fc0
BEOOM <wnsqja0512@naver.com>
2022-12-05 12:17:38 +0900

    #284 params 경로 확인하기

--- src/components/20/pages/ProductDetail.js
+++ src/components/20/pages/ProductDetail.js
@@ -1,4 +1,9 @@
+import { useParams } from "react-router-dom";
+
 const ProductDetail = () => {
+  const params = useParams();
+
+  console.log(params.productId);
   return (
     <section>
       <h1>Product Detail</h1>
