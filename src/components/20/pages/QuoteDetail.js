32c354cf54a124e987dca2a51e3ac7f28877c222
BEOOM <wnsqja0512@naver.com>
2022-12-06 11:38:31 +0900

    #293 데이터 출력 및 연결

--- src/components/20/pages/QuoteDetail.js
+++ src/components/20/pages/QuoteDetail.js
@@ -2,14 +2,25 @@ import { useParams } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import { Fragment } from "react";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "1", author: "Max", text: "Learning React is fun!" },
+  { id: "2", author: "Maximilian", text: "Learning React is great!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote Found</p>;
+  }
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
-      <Route path="/quotes/:quoteId/comments">
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
     </Fragment>
