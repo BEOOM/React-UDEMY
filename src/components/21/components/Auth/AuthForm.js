2402d76d251a2c2e9a62fa1796f6625cff4570d1
BEOOM <wnsqja0512@naver.com>
2022-12-10 11:41:49 +0900

    #317  사용자 피드백 보여주기

--- src/components/21/components/Auth/AuthForm.js
+++ src/components/21/components/Auth/AuthForm.js
@@ -6,6 +6,7 @@ const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false)
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -15,6 +16,8 @@ const AuthForm = () => {
     e.preventDefault();
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
+
+    setIsLoading(true)
 
     if (isLogin) {
     } else {
@@ -32,11 +35,15 @@ const AuthForm = () => {
           },
         }
       ).then((res) => {
+        setIsLoading(false)
         if (res.ok) {
         } else {
           res.json().then((data) => {
-            //show error modal
-            console.log(data);
+            let errorMessage = "Authentication failed";
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+            alert(errorMessage);
           });
         }
       });
@@ -61,7 +68,8 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+         {!isLoading &&<button>{isLogin ? "Login" : "Create Account"}</button>}
+         {isLoading && <p>Sending Request...</p>}
           <button
             type="button"
             className={classes.toggle}
