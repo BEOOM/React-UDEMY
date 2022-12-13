4271f187d4188c8ae96c98fe4392c9bc7aa299b5
BEOOM <wnsqja0512@naver.com>
2022-12-13 12:11:50 +0900

    #321 사용자 redirect

--- src/components/21/components/Auth/AuthForm.js
+++ src/components/21/components/Auth/AuthForm.js
@@ -1,8 +1,10 @@
 import { useState, useRef, useContext } from "react";
 import AuthContext from "../../store/auth-context";
 import classes from "./AuthForm.module.css";
+import { useHistory } from "react-router-dom";
 
 const AuthForm = () => {
+  const history = useHistory();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
@@ -56,6 +58,8 @@ const AuthForm = () => {
       })
       .then((data) => {
         authCtx.login(data.idToken);
+        history.replace('/')
+        //이전 페이지로 갈 수 없음
       })
       .catch((err) => {
         alert(err.message);
--- src/components/21/components/Profile/ProfileForm.js
+++ src/components/21/components/Profile/ProfileForm.js
@@ -1,8 +1,10 @@
 import classes from "./ProfileForm.module.css";
 import { useRef, useContext } from "react";
 import AuthContext from "../../store/auth-context";
+import { useHistory } from "react-router-dom";
 
 const ProfileForm = () => {
+  const history = useHistory();
   const newPasswordInputRef = useRef();
   const authCtx = useContext(AuthContext);
 
@@ -23,9 +25,9 @@ const ProfileForm = () => {
           "Content-Type": "application/json",
         },
       }
-    ).then(res => {
-      
-    })
+    ).then((res) => {
+      history.replace("/");
+    });
   };
   return (
     <form className={classes.form} onSubmit={submitHandler}>
