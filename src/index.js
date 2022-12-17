52336660d0730f4369e3a4afc2dfe428b73038b7
BEOOM <wnsqja0512@naver.com>
2022-12-17 11:42:36 +0900

    Section24 - CSS

--- src/index.js
+++ src/index.js
@@ -20,11 +20,8 @@ import Section20 from "./components/20/Section20";
 import { BrowserRouter } from "react-router-dom";
 import Section21 from "./components/21/Section21";
 import Section28 from "./components/28/Section28";
+import Section24 from "./components/24/Section24";
 import AuthContextProvider from "./components/28/context/auth-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <AuthContextProvider>
-    <Section28 />
-  </AuthContextProvider>
-);
+root.render(<Section24 />);
